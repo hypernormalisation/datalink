0010f6ec2ba7ca91c51fc7a1af10ae62107a2927
Stephen Ogilvy <sogilvy@protonmail.com>
2020-09-17 17:38:18 +0100

    version increment

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='datalink',
-    version='0.1.1',
+    version='0.1.2',
 
     description=('Create simple interfaces to SQL that make working with data as simple'
                 ' as working with dictionaries.'),
